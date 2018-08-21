2902b7f0927a85f8a849568d076d166162341d01
jessym04 <42000362+jessym04@users.noreply.github.com>
2018-08-21 00:54:15 -0400

    Add files via upload

--- popup.py
+++ popup.py
@@ -1,21 +1,12 @@
 import random
+from random import randint
+
+good =  ["doinggreat.png","keepswim.png", "nomount.png"]
+love = ["doinggreat.png", "deservebest.png", "worth.png"]
+laugh = ["belikethat.png", "worth.png"]
+wow = ["doit.png", "keepswim.png"]
+sad = ["believe.png", "deservebest.png", "doit.png", "failfast.png", "keepswim.png", "nomount.png", "worth.png"]
+mad = ["belikethat.png", "failfast.png", "nomount.png", "worth.png"]
 
 def getaff(mood):
-  good =  ["doinggreat.png","keepswim.png", "nomount.png"]
-  love = ["doinggreat.png", "deservebest.png", "worth.png"]
-  laugh = ["belikethat.png", "worth.png"]
-  wow = ["doit.png", "keepswim.png"]
-  sad = ["believe.png", "deservebest.png", "doit.png", "failfast.png", "keepswim.png", "nomount.png", "worth.png"]
-  mad = ["belikethat.png", "failfast.png", "nomount.png", "worth.png"]
-  if mood == "good":
-    return good[randint(0,len(good)-1)]
-  if mood == "love":
-    return love[randint(0,len(love)-1)]
-  if mood == "laugh":
-    return laugh[randint(0,len(laugh)-1)]
-  if mood == "wow":
-    return wow[randint(0,len(wow)-1)]
-  if mood == "sad":
-    return sad[randint(0,len(randint)-1)]
-  if mood == "mad":
-    return mad[randint(0,len(randint)-1)]
+  return mood [random.randint(0,len(mood)-1)]
